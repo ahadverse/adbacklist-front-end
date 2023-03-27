4aac8e284e72198cb8168bad553877d164d881e3
Ahad69 <ahadxx99@gmail.com>
2023-03-27 16:06:46 +0600

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,24 @@
 import "@/styles/globals.css";
-import { useRouter } from "next/router";
 import Script from "next/script";
-import { useEffect } from "react";
-import * as gtag from "../component/lib/gtag"
 
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <>
       <Script
-        async
-        strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=UA-156492667-1"
-      />
-      <Script
-        id="google-analytics"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-156492667-1', {
-        page_path: window.location.pathname,
-         });
-       `,
-        }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-156492667-1');
+        `}
+      </Script>
 
       <Component {...pageProps} />
     </>
