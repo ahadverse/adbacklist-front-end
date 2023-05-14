9393ff7674e953589e80ce90fc9f6dfdecd6340f
Ahad69 <ahadxx99@gmail.com>
2023-05-14 22:35:40 +0600

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -6,14 +6,23 @@ class WebDocument extends Document {
       <Html lang="en" class="notranslate" translate="no">
         <Head>
       
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-M31GFLRX0Q"></Script>
-<Script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
 
-  gtag('config', 'G-M31GFLRX0Q');
-</Script>
+
+<Script
+      async src="https://www.googletagmanager.com/gtag/js?id=G-M31GFLRX0Q"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-M31GFLRX0Q');
+        `}
+      </Script>
+
+
         <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
